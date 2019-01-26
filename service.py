a2cbcc6625bfd27e7453e5a63220228b3002b90e
zac <zgallagher29@gmail.com>
2019-01-26 14:15:01 -0600

    trying out google cloud and docker

---
+++ service.py
@@ -0,0 +1,23 @@
+import requests 
+  
+# api-endpoint 
+URL = "http://maps.googleapis.com/maps/api/geocode/json"
+  
+# location given here 
+location = "delhi technological university"
+  
+# defining a params dict for the parameters to be sent to the API 
+PARAMS = {'address':location} 
+  
+# sending get request and saving the response as response object 
+r = requests.get(url = URL, params = PARAMS) 
+  
+# extracting data in json format 
+data = r.json() 
+  
+  
+formatted_address = data['results'][0]['formatted_address'] 
+  
+# printing the output 
+print("Latitude:%s\nLongitude:%s\nFormatted Address:%s"
+      %(formatted_address)) 
--- testScript_DocstoSheets.py
+++ testScript_DocstoSheets.py
@@ -21,3 +21,44 @@ file_id = files[1]['id']
 
 
 # ideas: read the file into dataframe and then maybe match some rows with common words and put them into note card?
+from apiclient import errors
+from apiclient import http
+# ...
+
+def print_file_metadata(service, file_id):
+  """Print a file's metadata.
+
+  Args:
+    service: Drive API service instance.
+    file_id: ID of the file to print metadata for.
+  """
+  try:
+    file = service.files().get(fileId=file_id).execute()
+
+    print ('Title: %s' % file['title'])
+    print ('MIME type: %s' % file['mimeType'])
+  except ():
+    print ('An error occurred: %s' % error)
+
+
+print_file_metadata(DRIVE,file_id)
+Uses the Python client library.
+
+from apiclient import errors
+# ...
+
+def print_about(service):
+  """Print information about the user along with the Drive API settings.
+
+  Args:
+    service: Drive API service instance.
+  """
+  try:
+    about = service.about().get().execute()
+
+    print 'Current user name: %s' % about['name']
+    print 'Root folder ID: %s' % about['rootFolderId']
+    print 'Total quota (bytes): %s' % about['quotaBytesTotal']
+    print 'Used quota (bytes): %s' % about['quotaBytesUsed']
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
